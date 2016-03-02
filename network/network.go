78e42f85e59ef83a05c6ce82bbe4862d38952545
Lars André Kristiansen <larsakr@gmail.com>
2016-03-02 12:28:55 +0100

    New implementation of network package. More code for error handling is need.

--- network/network.go
+++ network/network.go
@@ -1,110 +1,146 @@
 package network
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"log"
 	"net"
 )
 
-// TODO: Do we stil need this?
-// type Frame struct {
-// 	RemoteAddr string
-// 	Data       []byte
-// }
-
-type clientConn struct {
+type peerConn struct {
+	raddr  string
 	conn   *net.TCPConn
 	recvch chan string
 	sendch chan string
+
+	br *bufio.Reader
+
+	parent *Peer
 }
 
-var connectionList struct {
-	m     map[string]*clientConn
-	addch chan *clientConn
-	rmch  chan *clientConn
+type Peer struct {
+	port     int
+	m        map[string]*peerConn
+	addch    chan *peerConn
+	removech chan *peerConn
+
+	Receive chan Frame
+	Send    chan Frame
 }
 
-func Init(port int) error {
-	connectionList.m = make(map[string]*clientConn)
-	connectionList.addch = make(chan *clientConn)
-	connectionList.rmch = make(chan *clientConn)
+type Frame struct {
+	Raddr string
+	Data  string
+}
+
+func NewPeer(port int) (*Peer, error) {
+	p := &Peer{
+		port:     port,
+		m:        make(map[string]*peerConn),
+		addch:    make(chan *peerConn),
+		removech: make(chan *peerConn),
+		Receive:  make(chan Frame),
+		Send:     make(chan Frame),
+	}
+
+	go p.peerLoop()
 
 	laddr, err := ResolveLocalAddr(port)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	listener, err := net.ListenTCP("tcp4", laddr)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	go listenerLoop(listener)
-	
-	fmt.Printf("Listening on port %v.\n", port)
-	
-	return nil
+	go p.listenerLoop(listener)
+
+	log.Printf("Listening on port %v.\n", port)
+
+	return p, nil
 }
 
-func listenerLoop(ln *net.TCPListener) {
-	
+// Responsible for synchronising access to the connection list data
+// structure. Runs in its own goroutine.
+func (p *Peer) peerLoop() {
+	for {
+		select {
+		case fr := <-p.Send:
+			// lookup the peerConn and forward
+			// TODO: Create connection if it doesn't exist
+			pc, ok := p.m[fr.Raddr]
+			if !ok {
+				log.Printf("No connection to %v. Message dropped.\n", fr.Raddr)
+				continue
+			}
+			pc.sendch <- fr.Data
+
+		case pc := <-p.addch:
+			p.m[pc.raddr] = pc
+			log.Printf("Added connection to %v.\n", pc.raddr)
+		case pc := <-p.removech:
+			delete(p.m, pc.raddr)
+		}
+	}
+}
+
+// Listens for new connections, and starts goroutines to handle them.
+func (p *Peer) listenerLoop(ln *net.TCPListener) {
+
 	for {
 		conn, err := ln.AcceptTCP()
 		if err != nil {
 			panic(err)
 		}
 
-		fmt.Printf("Accepted connection from %v.\n", conn.RemoteAddr())
+		log.Printf("Accepted connection from %v.\n", conn.RemoteAddr())
 
 		// TODO: check that this works correctly.
-		cc := &clientConn{
-			conn: conn,
+		pc := &peerConn{
+			raddr:  conn.RemoteAddr().String(),
+			conn:   conn,
 			recvch: make(chan string),
 			sendch: make(chan string),
+			br:     bufio.NewReader(conn),
+			parent: p,
 		}
-		connectionList.addch <- cc
-		
-		//go cc.readLoop()
-		//go cc.sendLoop()
+		p.addch <- pc
+
+		go pc.readLoop()
+		go pc.sendLoop()
+
+		pc.sendch <- fmt.Sprintf("Hello from %v.\n", conn.LocalAddr())
 	}
 }
 
-func (cc *clientConn) readLoop() {
-	// buf := make([]byte, 1024)
-	// raddr := conn.RemoteAddr().String()
-	// for {
-	// 	n, err := conn.Read(buf)
-	// 	if err != nil {
-	// 		connectionList.Lock()
-	// 		conn.Close()
-	// 		delete(connectionList.m, raddr)
-	// 		connectionList.Unlock()
-	// 		return
-	// 	}
-	// 
-	// 	fr := Frame{
-	// 		RemoteAddr: raddr,
-	// 		Data:       make([]byte, n),
-	// 	}
-	// 	copy(fr.Data, buf)
-	// 
-	// 	recvch <- fr
-	// }
+func (pc *peerConn) readLoop() {
+	for {
+		s, err := pc.br.ReadString('\n')
+		if err != nil {
+			pc.conn.Close()
+			log.Printf("Connection from %v closed.\n", pc.raddr)
+			pc.parent.removech <- pc
+			return
+		}
+
+		pc.parent.Receive <- Frame{pc.raddr, s}
+	}
 }
 
-func (cc *clientConn) sendLoop() {
-	// for {
-	// 	fr := <-sendch
-	// 	_, ok := connectionList.m[fr.RemoteAddr]
-	// 	if !ok {
-	// 		panic("Trying to send to host without connection.")
-	// 	}
-	// 
-	// 	connectionList.RLock()
-	// 	conn := connectionList.m[fr.RemoteAddr]
-	// 	conn.Write(fr.Data)
-	// 	connectionList.RUnlock()
-	// }
+func (pc *peerConn) sendLoop() {
+	for {
+		s := <-pc.sendch
+		_, err := pc.conn.Write([]byte(s))
+		if err != nil {
+			log.Printf("Failed in sendLoop.\n")
+			pc.parent.removech <- pc
+		}
+
+		log.Printf("Message sent to %v:\n%v", pc.raddr, s)
+	}
 }
 
 // Finds the local address by searching through the network interfaces
--- network/network_test.go
+++ network/network_test.go
@@ -2,15 +2,28 @@ package network
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"testing"
 )
 
 func TestMain(m *testing.M) {
 	laddr, _ := ResolveLocalAddr(6000)
-	fmt.Printf("This computer has local address: %v\n", laddr)
+	log.Printf("This computer has local address: %v\n", laddr)
 
-	Init(6000)
-	select {}
+	p, _ := NewPeer(6000)
+
+	for {
+		fr := <-p.Receive
+		log.Printf("Received from %v:\n%v", fr.Raddr, fr.Data)
+	}
+
+	fmt.Printf("\nConnections:\n")
+	for ip, _ := range p.m {
+		fmt.Println(ip)
+	}
+
+	os.Exit(0)
 }
 
 func TestResolveLocalAddr(t *testing.T) {
