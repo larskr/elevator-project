4680908404a6301a9a0be09166e2f3ec54641aa2
Student <student@itk-d650>
2016-03-04 12:58:27 +0100

    Minor fixes

--- driver/driver_test.go
+++ driver/driver_test.go
@@ -1,119 +1,116 @@
 package driver
 
 import (
-	"testing"
 	"fmt"
 	"os"
+	"testing"
 	"time"
 )
 
 type Orders struct {
-     N             int
-     oType         [10] elev_button_type
-     oFloor        [10] int     
+	N      int
+	oType  [10]elev_button_type
+	oFloor [10]int
 }
 
-func addOrder(button elev_button, orders *Orders){
-     orders.oFloor[orders.N] = button.floor
-     orders.oType[orders.N]  = button.bType
-     orders.N++
-     fmt.Printf("Added Floor %v  Button %v \n", button.floor,button.bType)
+func addOrder(button elev_button, orders *Orders) {
+	orders.oFloor[orders.N] = button.floor
+	orders.oType[orders.N] = button.bType
+	orders.N++
+	fmt.Printf("Added Floor %v  Button %v \n", button.floor, button.bType)
 }
 
-func removeOrder(orders* Orders){
-     for i:=0; i<orders.N; i++ {
-     orders.oFloor[i]=orders.oFloor[i+1]
-     orders.oType[i]=orders.oType[i+1]
-     }
-     orders.N--
+func removeOrder(orders *Orders) {
+	for i := 0; i < orders.N; i++ {
+		orders.oFloor[i] = orders.oFloor[i+1]
+		orders.oType[i] = orders.oType[i+1]
+	}
+	orders.N--
 }
-
 
 func readElevatorInput(orders *Orders) {
-//Read buttons
-var prev = [N_FLOORS][N_BUTTONS]int{}
-m := 0
-button := elev_button{0,0}
+	//Read buttons
+	var prev = [N_FLOORS][N_BUTTONS]int{}
+	m := 0
+	button := elev_button{0, 0}
 
-    for {
-        for button.floor=0; button.floor<N_FLOORS; button.floor++{
-	    for button.bType=0; button.bType<N_BUTTONS; button.bType++{
-	        m=Elev_get_button_signal(button.bType,button.floor)
-	        if m!= 0 && m!= prev[button.floor][button.bType] {
-	            addOrder(button,orders)
-                    fmt.Printf("Floor %v  Button %v", button.floor,button.bType)
-                }  
-                prev[button.floor][button.bType]=m
-            }
-	}    
-    }
+	for {
+		for button.floor = 0; button.floor < N_FLOORS; button.floor++ {
+			for button.bType = 0; button.bType < N_BUTTONS; button.bType++ {
+				m = Elev_get_button_signal(button.bType, button.floor)
+				if m != 0 && m != prev[button.floor][button.bType] {
+					addOrder(button, orders)
+					//fmt.Printf("Floor %v  Button %v", button.floor, button.bType)
+				}
+				prev[button.floor][button.bType] = m
+			}
+		}
+	}
 }
 
-
 func TestMain(m *testing.M) {
-    Elev_init();
-    fmt.Println("Press STOP button to stop elevator and exit program.");
-    orders := Orders{}
-    orders.N = 0
-    var requestedFloor int
-    var floor int = 0
-    // go startElev()
-    // for {}
-    
-    Elev_set_motor_direction(DIRN_UP)
-    time.Sleep(1*time.Second)
-    
-   /* go func() {
-    	var signal, floor int
-    	for {
-    		signal = Elev_get_floor_sensor_signal()
-    		if signal != -1 && signal != floor {
-    			floor = signal
-    			fmt.Printf("I am on floor %v!\n", floor)
-    			if floor == 0 {
-    				time.Sleep(250*time.Millisecond)
-    				Elev_set_motor_direction(DIRN_STOP)
-    			}
-    		}
-    		time.Sleep(25*time.Millisecond)
-    	}
-    }()*/
-    
-    go readElevatorInput(&orders)
-    for {
-    	time.Sleep(25*time.Millisecond)
-         requestedFloor=orders.oFloor[0]
-         var signal int = Elev_get_floor_sensor_signal()
-         signal = Elev_get_floor_sensor_signal()
-         if signal != -1 {
-         floor=signal
-         }
-         if orders.N != 0 {
-	 	if floor > requestedFloor {
-	 	        Elev_set_motor_direction(DIRN_DOWN) 
-	 	} else if floor < requestedFloor {
-	 	        Elev_set_motor_direction(DIRN_UP)
-		} else if floor == requestedFloor{
-	 	    fmt.Printf("Reached floor %v \n", requestedFloor)
-	 	    removeOrder(&orders)
-	 	}
-         } else {
-               Elev_set_motor_direction(DIRN_STOP)
-         }
-        
-            
-        /*// Change direction when we reach top/bottom floor
-        if Elev_get_floor_sensor_signal() == N_FLOORS - 1 {
-           Elev_set_motor_direction(DIRN_DOWN)
-        } else if Elev_get_floor_sensor_signal() == 0 {
-            Elev_set_motor_direction(DIRN_UP)
-        }
-*/
-        // Stop elevator and exit program if the stop button is pressed
-        time.Sleep(25*time.Millisecond)
-        if Elev_get_stop_signal()!=0 {
-            Elev_set_motor_direction(DIRN_STOP)
-        }
-    }
+	Elev_init()
+	fmt.Println("Press STOP button to stop elevator and exit program.")
+	orders := Orders{}
+	orders.N = 0
+	var requestedFloor int
+	var floor int = 0
+	// go startElev()
+	// for {}
+
+	Elev_set_motor_direction(DIRN_UP)
+	time.Sleep(1 * time.Second)
+
+	/* go func() {
+		var signal, floor int
+		for {
+			signal = Elev_get_floor_sensor_signal()
+			if signal != -1 && signal != floor {
+				floor = signal
+				fmt.Printf("I am on floor %v!\n", floor)
+				if floor == 0 {
+					time.Sleep(250*time.Millisecond)
+					Elev_set_motor_direction(DIRN_STOP)
+				}
+			}
+			time.Sleep(25*time.Millisecond)
+		}
+	}()*/
+
+	go readElevatorInput(&orders)
+	for {
+		time.Sleep(25 * time.Millisecond)
+		requestedFloor = orders.oFloor[0]
+		var signal int = Elev_get_floor_sensor_signal()
+		signal = Elev_get_floor_sensor_signal()
+		if signal != -1 {
+			floor = signal
+		}
+		if orders.N != 0 {
+			if floor > requestedFloor {
+				Elev_set_motor_direction(DIRN_DOWN)
+			} else if floor < requestedFloor {
+				Elev_set_motor_direction(DIRN_UP)
+			} else if floor == requestedFloor {
+				fmt.Printf("Reached floor %v \n", requestedFloor)
+				removeOrder(&orders)
+			}
+		} else {
+			Elev_set_motor_direction(DIRN_STOP)
+		}
+
+		/*// Change direction when we reach top/bottom floor
+		  if Elev_get_floor_sensor_signal() == N_FLOORS - 1 {
+		     Elev_set_motor_direction(DIRN_DOWN)
+		  } else if Elev_get_floor_sensor_signal() == 0 {
+		      Elev_set_motor_direction(DIRN_UP)
+		  }
+		*/
+		// Stop elevator and exit program if the stop button is pressed
+		time.Sleep(25 * time.Millisecond)
+		if Elev_get_stop_signal() != 0 {
+			Elev_set_motor_direction(DIRN_STOP)
+		}
+	}
 	os.Exit(0)
 }
--- driver/elev.go
+++ driver/elev.go
@@ -1,163 +1,152 @@
 package driver
 
-
 type elev_motor_direction int
 
 const (
-    DIRN_DOWN elev_motor_direction     = -1 
-    DIRN_STOP                          =  0   
-    DIRN_UP                            =  1   
+	DIRN_DOWN elev_motor_direction = -1
+	DIRN_STOP                      = 0
+	DIRN_UP                        = 1
 )
 
 type elev_button_type int
 
+type elev_button struct {
+	bType elev_button_type
+	floor int
+}
+
 const (
-    BUTTON_CALL_UP elev_button_type     = 0 
-    BUTTON_CALL_DOWN                    = 1   
-    BUTTON_COMMAND                      = 2   
+	BUTTON_CALL_UP   elev_button_type = 0
+	BUTTON_CALL_DOWN                  = 1
+	BUTTON_COMMAND                    = 2
 )
 
 const (
-	MOTOR_SPEED = 2800 
-        N_FLOORS    = 4
-        N_BUTTONS   = 3
-	)
+	MOTOR_SPEED = 2800
+	N_FLOORS    = 4
+	N_BUTTONS   = 3
+)
+
 var lamp_channel_matrix = [N_FLOORS][N_BUTTONS]int{
-    {LIGHT_UP1, LIGHT_DOWN1, LIGHT_COMMAND1},
-    {LIGHT_UP2, LIGHT_DOWN2, LIGHT_COMMAND2},
-    {LIGHT_UP3, LIGHT_DOWN3, LIGHT_COMMAND3},
-    {LIGHT_UP4, LIGHT_DOWN4, LIGHT_COMMAND4},
-};
+	{LIGHT_UP1, LIGHT_DOWN1, LIGHT_COMMAND1},
+	{LIGHT_UP2, LIGHT_DOWN2, LIGHT_COMMAND2},
+	{LIGHT_UP3, LIGHT_DOWN3, LIGHT_COMMAND3},
+	{LIGHT_UP4, LIGHT_DOWN4, LIGHT_COMMAND4},
+}
 
-
-var button_channel_matrix = [N_FLOORS][N_BUTTONS]int {
-    {BUTTON_UP1, BUTTON_DOWN1, BUTTON_COMMAND1},
-    {BUTTON_UP2, BUTTON_DOWN2, BUTTON_COMMAND2},
-    {BUTTON_UP3, BUTTON_DOWN3, BUTTON_COMMAND3},
-    {BUTTON_UP4, BUTTON_DOWN4, BUTTON_COMMAND4},
-};
-
-
+var button_channel_matrix = [N_FLOORS][N_BUTTONS]int{
+	{BUTTON_UP1, BUTTON_DOWN1, BUTTON_COMMAND1},
+	{BUTTON_UP2, BUTTON_DOWN2, BUTTON_COMMAND2},
+	{BUTTON_UP3, BUTTON_DOWN3, BUTTON_COMMAND3},
+	{BUTTON_UP4, BUTTON_DOWN4, BUTTON_COMMAND4},
+}
 
 func Elev_init() {
-    //init_success := Init()
-    Init()
-    //assert(init_success && "Unable to initialize elevator hardware!");
-    var b elev_button_type = 0
-    for f := 0; f < N_FLOORS; f++ {
-        for b = 0; b < N_BUTTONS; b++{
-            Elev_set_button_lamp(b, f, 0)
-        }
-    }
+	//init_success := Init()
+	Init()
+	//assert(init_success && "Unable to initialize elevator hardware!");
+	var b elev_button_type = 0
+	for f := 0; f < N_FLOORS; f++ {
+		for b = 0; b < N_BUTTONS; b++ {
+			Elev_set_button_lamp(b, f, 0)
+		}
+	}
 
-    Elev_set_stop_lamp(0)
-    Elev_set_door_open_lamp(0)
-    Elev_set_floor_indicator(0)
+	Elev_set_stop_lamp(0)
+	Elev_set_door_open_lamp(0)
+	Elev_set_floor_indicator(0)
 }
-
 
 func Elev_set_motor_direction(dirn elev_motor_direction) {
-    if dirn == 0{
-        Write_analog(MOTOR, 0)
-    } else if dirn > 0 {
-        Clear_bit(MOTORDIR)
-        Write_analog(MOTOR, MOTOR_SPEED)
-    } else if dirn < 0 {
-        Set_bit(MOTORDIR)
-        Write_analog(MOTOR, MOTOR_SPEED)
-    }
+	if dirn == 0 {
+		Write_analog(MOTOR, 0)
+	} else if dirn > 0 {
+		Clear_bit(MOTORDIR)
+		Write_analog(MOTOR, MOTOR_SPEED)
+	} else if dirn < 0 {
+		Set_bit(MOTORDIR)
+		Write_analog(MOTOR, MOTOR_SPEED)
+	}
 }
 
+func Elev_set_button_lamp(button elev_button_type, floor int, value int) {
+	//    assert(floor >= 0);
+	//    assert(floor < N_FLOORS);
+	//    assert(button >= 0);
+	//    assert(button < N_BUTTONS);
 
-func Elev_set_button_lamp(button elev_button_type,floor int,value int) {
-//    assert(floor >= 0);
-//    assert(floor < N_FLOORS);
-//    assert(button >= 0);
-//    assert(button < N_BUTTONS);
-
-    if value!=0 {
-        Set_bit(lamp_channel_matrix[floor][button])
-    } else {
-        Clear_bit(lamp_channel_matrix[floor][button])
-    }
+	if value != 0 {
+		Set_bit(lamp_channel_matrix[floor][button])
+	} else {
+		Clear_bit(lamp_channel_matrix[floor][button])
+	}
 }
-
 
 func Elev_set_floor_indicator(floor int) {
-//    assert(floor >= 0);
-//    assert(floor < N_FLOORS);
+	//    assert(floor >= 0);
+	//    assert(floor < N_FLOORS);
 
-    // Binary encoding. One light must always be on.
-    if floor != 0 & 0x02 {
-        Set_bit(LIGHT_FLOOR_IND1)
-    } else {
-        Clear_bit(LIGHT_FLOOR_IND1)
-    }    
+	// Binary encoding. One light must always be on.
+	if floor != 0&0x02 {
+		Set_bit(LIGHT_FLOOR_IND1)
+	} else {
+		Clear_bit(LIGHT_FLOOR_IND1)
+	}
 
-    if floor != 0 & 0x01 {
-        Set_bit(LIGHT_FLOOR_IND2)
-    } else {
-        Clear_bit(LIGHT_FLOOR_IND2)
-    }   
+	if floor != 0&0x01 {
+		Set_bit(LIGHT_FLOOR_IND2)
+	} else {
+		Clear_bit(LIGHT_FLOOR_IND2)
+	}
 }
-
 
 func Elev_set_door_open_lamp(value int) {
-    if value != 0 {
-        Set_bit(LIGHT_DOOR_OPEN)
-    } else {
-        Clear_bit(LIGHT_DOOR_OPEN)
-    }
+	if value != 0 {
+		Set_bit(LIGHT_DOOR_OPEN)
+	} else {
+		Clear_bit(LIGHT_DOOR_OPEN)
+	}
 }
-
 
 func Elev_set_stop_lamp(value int) {
-    if value != 0 {
-        Set_bit(LIGHT_STOP)
-    } else {
-        Clear_bit(LIGHT_STOP)
-    }
+	if value != 0 {
+		Set_bit(LIGHT_STOP)
+	} else {
+		Clear_bit(LIGHT_STOP)
+	}
 }
 
+func Elev_get_button_signal(button elev_button_type, floor int) int {
+	//    assert(floor >= 0);
+	//    assert(floor < N_FLOORS);
+	//    assert(button >= 0);
+	//    assert(button < N_BUTTONS);
 
-
-func Elev_get_button_signal(button elev_button_type,floor int) int {
-//    assert(floor >= 0);
-//    assert(floor < N_FLOORS);
-//    assert(button >= 0);
-//    assert(button < N_BUTTONS);
-
-
-    if Read_bit(button_channel_matrix[floor][button])!=0 {
-        return 1
-    } else {
-        return 0
-    }    
+	if Read_bit(button_channel_matrix[floor][button]) != 0 {
+		return 1
+	} else {
+		return 0
+	}
 }
-
 
 func Elev_get_floor_sensor_signal() int {
-    if Read_bit(SENSOR_FLOOR1) !=0 {
-        return 0
-    } else if Read_bit(SENSOR_FLOOR2) !=0 {
-        return 1
-    } else if Read_bit(SENSOR_FLOOR3) !=0 {
-        return 2
-    } else if Read_bit(SENSOR_FLOOR4) !=0 {
-        return 3
-    } else {
-        return -1
-    }
+	if Read_bit(SENSOR_FLOOR1) != 0 {
+		return 0
+	} else if Read_bit(SENSOR_FLOOR2) != 0 {
+		return 1
+	} else if Read_bit(SENSOR_FLOOR3) != 0 {
+		return 2
+	} else if Read_bit(SENSOR_FLOOR4) != 0 {
+		return 3
+	} else {
+		return -1
+	}
 }
-
 
 func Elev_get_stop_signal() int {
-    return Read_bit(STOP);
+	return Read_bit(STOP)
 }
-
 
 func Elev_get_obstruction_signal() int {
-    return Read_bit(OBSTRUCTION);
+	return Read_bit(OBSTRUCTION)
 }
-
-
