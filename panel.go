c9dfbd22a70f774615d7b4292ffc77d38f3a68f9
Lars André Kristiansen <larsakr@gmail.com>
2016-03-15 14:54:50 +0100

    Removed unnecessary channels from Panel.

--- panel.go
+++ panel.go
@@ -13,50 +13,32 @@ type Request struct {
 	Direction elev.Direction
 }
 
-type lamp struct {
-	floor  int
-	button elev.Button
-	on     bool
-}
-
 type Panel struct {
 	Requests chan Request
 	Commands chan int
 
-	lamps  [elev.NumFloors][3]lamp
-	resetc chan lamp
+	lamps  [elev.NumFloors][3]bool
 }
 
 func (p *Panel) Start() {
 	p.Requests = make(chan Request, 2*elev.NumFloors)
 	p.Commands = make(chan int)
-	p.resetc = make(chan lamp, 2*elev.NumFloors)
 	go p.poll()
 }
 
 func (p *Panel) Reset(b elev.Button, floor int) {
-	p.resetc <- lamp{floor, b, false}
+	p.lamps[floor][b] = false
+	elev.SetButtonLamp(b, floor, 0)
 }
 
 func (p *Panel) poll() {
 	var prev [elev.NumFloors][3]int
 
 	for {
-		select {
-		case lamp := <-p.resetc:
-			p.lamps[lamp.floor][lamp.button].on = lamp.on
-			if lamp.on {
-				elev.SetButtonLamp(lamp.button, lamp.floor, 1)
-			} else {
-				elev.SetButtonLamp(lamp.button, lamp.floor, 0)
-			}
-		default:
-		}
-
 		for floor := 0; floor < elev.NumFloors; floor++ {
 			v := elev.ReadButton(elev.CallUp, floor)
 			if v != 0 && prev[floor][elev.CallUp] == 0 {
-				if !p.lamps[floor][elev.CallUp].on {
+				if !p.lamps[floor][elev.CallUp] {
 					p.Requests <- Request{
 						Floor:     floor,
 						Direction: elev.Up,
@@ -69,7 +51,7 @@ func (p *Panel) poll() {
 
 			v = elev.ReadButton(elev.CallDown, floor)
 			if v != 0 && v != prev[floor][elev.CallDown] {
-				if !p.lamps[floor][elev.CallDown].on {
+				if !p.lamps[floor][elev.CallDown] {
 					p.Requests <- Request{
 						Floor:     floor,
 						Direction: elev.Down,
@@ -82,7 +64,7 @@ func (p *Panel) poll() {
 
 			v = elev.ReadButton(elev.Command, floor)
 			if v != 0 && v != prev[floor][elev.Command] {
-				if !p.lamps[floor][elev.Command].on {
+				if !p.lamps[floor][elev.Command] {
 					select {
 					case p.Commands <- floor:
 						elev.SetButtonLamp(elev.Command, floor, 1)
