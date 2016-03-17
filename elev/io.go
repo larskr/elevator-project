c8c42e7c1241cc28a46d6033cedcbd8dfa6a8562
Lars André Kristiansen <larsakr@gmail.com>
2016-03-17 10:09:43 +0100

    Lab fix. Forgot to uncomment io.go.

--- elev/io.go
+++ elev/io.go
@@ -5,31 +5,31 @@ package elev
 #cgo LDFLAGS: -lcomedi -lm
 #include "io.h"
 */
-//import "C"
+import "C"
 
 func InitIO() int {
-	//return int(C.io_init())
+	return int(C.io_init())
 	return 0
 }
 
 func SetBit(channel int) {
-	//C.io_set_bit(C.int(channel))
+	C.io_set_bit(C.int(channel))
 }
 
 func ClearBit(channel int) {
-	//C.io_clear_bit(C.int(channel))
+	C.io_clear_bit(C.int(channel))
 }
 
 func WriteAnalog(channel int, value int) {
-	//C.io_write_analog(C.int(channel), C.int(value))
+	C.io_write_analog(C.int(channel), C.int(value))
 }
 
 func ReadBit(channel int) int {
-	//return int(C.io_read_bit(C.int(channel)))
+	return int(C.io_read_bit(C.int(channel)))
 	return 0
 }
 
 func ReadAnalog(channel int) int {
-	//return int(C.io_read_analog(C.int(channel)))
+	return int(C.io_read_analog(C.int(channel)))
 	return 0
 }
