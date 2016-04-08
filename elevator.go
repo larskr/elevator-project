4ee645eba83e09c61048708dec7f39422f461bb8
Lars André Kristiansen <larsakr@gmail.com>
2016-04-08 09:35:54 +0200

    Clean up of main.go and changes to elevator.go.

--- elevator.go
+++ elevator.go
@@ -20,7 +20,7 @@ type Elevator struct {
 
 	panel *Panel
 
-	// This map is used as set that holds the floor numbers which
+	// This map is used as a set that holds the floor numbers which
 	// has been pressed on command panel inside the elevator.
 	dest map[int]bool
 
@@ -43,8 +43,20 @@ func (e *Elevator) Start() {
 	go e.run()
 }
 
-func (e *Elevator) Add(req Request) {
+func (e *Elevator) AddRequest(req Request) {
 	e.queue <- req
+}
+
+func (e *Elevator) Requests() []Request {
+	reqs := make([]Request, 0, maxRequests)
+	for floor := 0; floor < elev.NumFloors; floor++ {
+		if e.requests[floor][indexOfDir(elev.Up)] {
+			reqs = append(reqs, Request{floor, elev.Up})
+		} else if e.requests[floor][indexOfDir(elev.Down)] {
+			reqs = append(reqs, Request{floor, elev.Down})			
+		}
+	}
+	return reqs		
 }
 
 func (e *Elevator) SimulateCost(req Request) float64 {
@@ -90,46 +102,41 @@ func moving(e *Elevator) stateFn {
 func atFloor(e *Elevator) stateFn {
 	elev.SetFloorIndicator(e.floor)
 
-	if len(e.dest) == 0 {
+	// Is this floor a destination?
+	if e.dest[e.floor] {
+		elev.SetMotorDirection(elev.Stop)
+		delete(e.dest, e.floor)
+
+		// Update lamps on internal elevator panel.
+		e.panel.SetLamp(elev.Command, e.floor, false)
+
+		// We clear all requests (up and down) if we stop. This assumes
+		// passengers will accept travelling in the wrong direction for a
+		// while.
+		e.clearRequest(e.floor, elev.Up)
+		e.clearRequest(e.floor, elev.Down)
+
+		return doorsOpen
+	}
+
+	// Is there a request at this floor in the direction we're going?
+	if e.requests[e.floor][indexOfDir(e.direction)] {
+		elev.SetMotorDirection(elev.Stop)
+		
+		e.clearRequest(e.floor, elev.Up)
+		e.clearRequest(e.floor, elev.Down)
+		
+		return doorsOpen
+	}
+
+	// No more destinations, and no more requests in the direction we are going.
+	if len(e.dest) == 0 && !e.hasWork() {
 		elev.SetMotorDirection(elev.Stop)
 		e.direction = elev.Stop
 		return idle
 	}
 
-	// Is this floor a destination?
-	// Is there a request at this floor in the direction we're going?
-	if e.dest[e.floor] || e.requests[e.floor][indexOfDir(e.direction)] {
-		elev.SetMotorDirection(elev.Stop)
-
-		if e.dest[e.floor] {
-			delete(e.dest, e.floor)
-
-			// Update lamps on internal elevator panel.
-			e.panel.SetLamp(elev.Command, e.floor, false)
-
-			// We clear all requests (up and down) if we stop. This assumes
-			// passengers will accept travelling in the wrong direction for a
-			// while.
-			e.clearRequest(e.floor, elev.Up)
-			e.clearRequest(e.floor, elev.Down)
-		}
-
-		// If we are only clearing requests in the direction of motion, we can
-		// do the following:
-		//                               (insert opposite dir here)
-		//                                            v
-		// if len(e.dest) == 0 && e.request[e.floor][...] {
-		//         e.clearRequest(e.floor, ...)
-		//         e.direction = ...
-		// }
-
-		if e.requests[e.floor][indexOfDir(e.direction)] {
-			e.clearRequest(e.floor, e.direction)
-		}
-
-		return doorsOpen
-	}
-
+	// Fail safe; should never be true.
 	if (e.direction == elev.Up && e.floor == elev.NumFloors-1) ||
 		(e.direction == elev.Down && e.floor == 0) {
 		elev.SetMotorDirection(elev.Stop)
@@ -210,7 +217,6 @@ func idle(e *Elevator) stateFn {
 				e.direction = elev.Down
 			}
 
-			e.dest[floor] = true
 			return gotoFloor
 		}
 	}
@@ -220,6 +226,20 @@ func idle(e *Elevator) stateFn {
 	return idle
 }
 
+// Checks if there are more requests in the current direction of motion.
+func (e *Elevator) hasWork() bool {
+	for floor := 0; floor < elev.NumFloors; floor++ {
+		if e.requests[floor][indexOfDir(e.direction)] &&
+			((e.direction == elev.Up && floor > e.floor) ||
+			(e.direction == elev.Down && floor < e.floor)) {
+				return true
+			
+		}
+	}
+	return false
+}
+
+// Clear requests and resets panel lamp.
 func (e *Elevator) clearRequest(floor int, dir elev.Direction) {
 	if (floor == 0 && dir == elev.Down) || (floor == elev.NumFloors-1 && dir == elev.Up) {
 		return // invalid request
---
+++ msgdata.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"encoding"
+	"encoding/binary"
+	"time"
+
+	"elevator-project/network"
+)
+
+func packData(p []byte, data encoding.BinaryMarshaler) int {
+	buf, _ := data.MarshalBinary()
+	return copy(p, buf)
+}
+
+func unpackData(p []byte, data encoding.BinaryUnmarshaler) {
+	data.UnmarshalBinary(p)
+}
+
+// sendData sends a message with the data and returns the message ID.
+// The data struct to be sent must be implmented in packData/unpackData.
+func sendData(node *network.Node, mtype network.MsgType, data encoding.BinaryMarshaler) uint32 {
+	var buf [network.MaxDataLength]byte
+
+	databuf, _ := data.MarshalBinary()
+	n := copy(buf[:], databuf)
+	msg := network.NewMessage(mtype, buf[:n])
+
+	node.SendMessage(msg)
+	return msg.ID
+}
+
+const (
+	PANEL  network.MsgType = 0x10
+	COST   network.MsgType = 0x11
+	ASSIGN network.MsgType = 0x12
+	BACKUP network.MsgType = 0x13
+)
+
+type panelData struct {
+	floor  int
+	button elev.Button
+	on     bool
+}
+
+type costData struct {
+	elevator network.Addr
+	req      Request
+	cost     float64
+}
+
+type assignData struct {
+	elevator network.Addr
+	req      Request
+	taken    bool
+}
+
+type backupData struct {
+	elevator network.Addr
+	created  time.Time
+	requests []Request
+}
+
+func (d *panelData) MarshalBinary() ([]byte, error) {
+	p := make([]byte, 12)
+	binary.BigEndian.PutUint32(p[:], uint32(d.floor))
+	binary.BigEndian.PutUint32(p[4:], uint32(d.button))
+	if d.on {
+		binary.BigEndian.PutUint32(p[8:], 1)
+	} else {
+		binary.BigEndian.PutUint32(p[8:], 0)
+	}
+	return p, nil
+}
+
+func (d *panelData) UnmarshalBinary(p []byte) error {
+	d.floor = int(binary.BigEndian.Uint32(p[:]))
+	d.button = elev.Button(binary.BigEndian.Uint32(p[4:]))
+	if binary.BigEndian.Uint32(p[8:]) == 1 {
+		d.on = true
+	}
+	return nil
+}
+
+func (d *costData) MarshalBinary() ([]byte, error) {
+	p := make([]byte, 32)
+	copy(p[:], d.elevator[:])
+	binary.BigEndian.PutUint32(p[16:], uint32(d.req.Floor))
+	binary.BigEndian.PutUint32(p[20:], uint32(d.req.Direction))
+	binary.BigEndian.PutUint64(p[24:], math.Float64bits(d.cost))
+	return p, nil
+}
+
+func (d *costData) UnmarshalBinary(p []byte) error {
+	copy(d.elevator[:], p[:])
+	d.req.Floor = int(binary.BigEndian.Uint32(p[16:]))
+	d.req.Direction = elev.Direction(binary.BigEndian.Uint32(p[20:]))
+	d.cost = math.Float64frombits(binary.BigEndian.Uint64(p[24:]))
+	return nil
+}
+
+func (d *assignData) MarshalBinary() ([]byte, error) {
+	p := make([]byte, 28)
+	copy(p[:], d.elevator[:])
+	binary.BigEndian.PutUint32(p[16:], uint32(d.req.Floor))
+	binary.BigEndian.PutUint32(p[20:], uint32(d.req.Direction))
+	if d.taken {
+		binary.BigEndian.PutUint32(p[24:], 1)
+	} else {
+		binary.BigEndian.PutUint32(p[24:], 0)
+	}
+	return p, nil
+}
+
+func (d *assignData) UnmarshalBinary(p []byte) error {
+	copy(d.elevator[:], p[:])
+	d.req.Floor = int(binary.BigEndian.Uint32(p[16:]))
+	d.req.Direction = elev.Direction(binary.BigEndian.Uint32(p[20:]))
+	if binary.BigEndian.Uint32(p[24:]) == 1 {
+		d.taken = true
+	}
+}
+
+func (d *backupData) MarshalBinary() ([]byte, error) {
+	buf := make([]byte, 40+2*maxRequests)
+	p := buf
+	copy(p, d.elevator[:])
+	p = p[16:]
+	timebuf, _ := d.created.MarshalBinary()
+	nc = len(timebuf)
+	binary.BigEndian.PutUint8(p, uint8(nc))
+	copy(p[16:], timebuf)
+	p = p[nc+1:]
+	nreq := len(d.requests)
+	binary.BigEndian.PutUint8(p, uint8(nreq))
+	p = p[1:]
+	for i := 0; i < nreq; i++ {
+		binary.BigEndian.PutUint8(p, uint8(d.requests[i].Floor))
+		if d.requests[i].Direction == elev.Down {
+			binary.BigEndian.PutUint8(p[1:], uint8(255))
+		} else {
+			binary.BigEndian.PutUint8(p[1:], uint8(d.requests[i].Direction))
+		}
+		p = p[2:]
+	}
+	n = 16 + 1 + nc + 1 + nreq*2
+	return buf[:n], nil
+}
+
+func (d *backupData) UnmarshalBinary(p []byte) error {
+	copy(d.elevator[:], p)
+	p = p[16:]
+	ntime = int(binary.BigEndian.Uint8(p))
+	p = p[1:]
+	d.created.UnmarshalBinary(p[:ntime])
+	p = p[ntime:]
+	nreq = int(binary.BigEndian.Uint8(p))
+	p = p[1:]
+	d.requests = make([]Request, nreq)
+	for i := 0; i < nreq; i++ {
+		d.requests[i].Floor = int(binary.BigEndian.Uint8(p))
+		dir := binary.BigEndian.Uint8(p[1:])
+		if dir == 255 {
+			d.requests[i].Direction = elev.Down
+		} else {
+			d.requests[i].Direction = elev.Direction(dir)
+		}
+	}
+	return nil
+}
